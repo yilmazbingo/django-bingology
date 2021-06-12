fd893ad9e627cd56ca74877aae77575165aa427b
yilmaz <bingolyilmaz@hotmail.com>
2021-06-12 06:23:43 -0400

    env SECRET_KEY set

--- bingologyserver/settings.py
+++ bingologyserver/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's4yj53$gq@4+$foo=#rdus9tg7%s(x_9_e4%oh_cykz^9i*%ko'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
