2428ab486dc2e7b1756e4dc4dd1e67fe58efcad5
yilmaz <bingolyilmaz@hotmail.com>
2021-06-13 10:32:36 -0400

    alowed-hosts heroku added

--- bingologyserver/settings.py
+++ bingologyserver/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost","django-bingology.herokuapp"]
+ALLOWED_HOSTS = ["127.0.0.1","localhost","django-bingology.herokuapp.com"]
 
 
 # Application definition
